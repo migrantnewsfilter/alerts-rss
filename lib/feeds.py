4f9ff52e22e3a78fab9f689bf2f5e4af73a6be89
Nandan Rao <nandanmarkrao@gmail.com>
2016-11-11 18:33:43 +0100

    cleaning up and composing

--- lib/feeds.py
+++ lib/feeds.py
@@ -3,4 +3,4 @@ from pymongo import MongoClient
 def get_feeds(client):
     collection = client['newsfilter'].terms
     feeds =  collection.find_one({ '_id': 'feeds' })
-    return feeds['feeds']
+    return feeds.get('feeds')
--- lib/mongo.py
+++ lib/mongo.py
@@ -1,4 +1,4 @@
-from itertools import islice, takewhile, counto
+from itertools import islice, takewhile, count
 from pymongo import MongoClient, UpdateOne
 import dateutil.parser
 from datetime import datetime
